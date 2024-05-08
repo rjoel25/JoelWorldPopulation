d3686899f31ce1e908a80d728be8428a04b3522f
Ryan Joel <167253957+rjoel25@users.noreply.github.com>
2024-05-08 02:05:54 -0700

    Add files via upload

---
+++ R/WorldPopulation_Cleaning.R
@@ -0,0 +1,11 @@
+#' Country Populations
+#'
+#' This data set comprises UN data of the population of UN nations between the years 1950 and 2020.
+#'
+#' @format A data frame with over200 observations with 3 columns.
+#' \describe{
+#'    \item{Country}{The name of the UN country.}
+#'    \item{Year}{The year of the population observation.}
+#'    \item{Pop}{The population of that given country for a given year.}
+#' }
+"WorldPopulation"
